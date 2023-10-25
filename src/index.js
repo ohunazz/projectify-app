57a32873436dc948012bcaa88deb506e2c67994b
ohunazz <ohunazz@gmail.com>
2023-10-25 10:39:09 -0400

    Finish Handling-Error in Sign-Up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import express from "express";
 import { userRouter } from "./routes/user.route.js";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+import { GlobalError } from "./middlewares/global-error.middleware.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cookieParser());
 app.use(express.json());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use("/users", userRouter);
+app.use(GlobalError.handle);
 
 app.listen(PORT, () => {
     console.log("Server is running on", PORT);
