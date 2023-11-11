e4e601429ec3c9bbe53f259254341cf811b5a40a
toptechdiv <132863607+toptechdiv@users.noreply.github.com>
2023-11-10 20:40:25 -0500

    Revert "Normalizing"

--- src/index.js
+++ src/index.js
@@ -2,19 +2,15 @@ import express from "express";
 import { userRouter } from "./routes/user.route.js";
 import dotenv from "dotenv";
 import { GlobalError } from "./middlewares/global-error.middleware.js";
-import { projectRouter } from "./routes/project.route.js";
-import { teamMemberRouter } from "./routes/team-member.route.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use("/users", userRouter);
-app.use("/projects", projectRouter);
-app.use("/team-members", teamMemberRouter);
 app.use(GlobalError.handle);
 
 app.listen(PORT, () => {
